cbb2f5995c011bfef4566025fe71856459dcb60a
MaelleT <maelle.taurand@ac-nantes.fr>
2017-11-06 21:43:24 +0100

    Fonctionnalité d'affichage des détails d'une tache (code non architecturé)

--- ToDoList/controllers/taskController.js
+++ ToDoList/controllers/taskController.js
@@ -5,7 +5,30 @@ exports.task_list = function(req, res, next) {
 
 // Display detail page for a specific Task
 exports.task_detail = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: Detail Task id : ' + req.params.id);
+
+    const pg = require('pg');
+    const connectionString = process.env.DATABASE_URL || 'postgres://postgres:Password1@192.168.1.50:5432/todo';
+
+    const db = new pg.Client(connectionString);
+    db.connect();
+    const query = {
+    // give the query a unique name
+    name: 'fetch-author',
+    text: 'SELECT * FROM task WHERE id = $1',
+    values: [req.params.id]
+    };
+
+    db.query(query, function(err, result){
+        if (err) {
+            console.log(err.stack);
+            res.send('ERROR');
+
+        } else {
+            console.log(result.rows[0]['name']);
+            res.send('Task detail: ' + req.params.id + ' Libelle : '+ result.rows[0]['libelle']);
+
+        }
+    });
 };
 
 // Handle Task create on POST
